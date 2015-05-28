3d08cf9f2287f24f6adbbebc6e5b5854c8291a11
Colm Britton <colmjude@gmail.com>
2015-05-28 12:22:30 +0100

    remove unneccessary file

--- app/assets/javascripts/modules/fauxdetails.js
+++
@@ -1,9 +0,0 @@
-;(function($) {
-  $(function() {
-      // Add conditional classname based on support
-      //$('html').addClass($.fn.details.support ? 'details' : 'no-details');
-
-      // Emulate <details> where necessary and enable open/close event handlers
-      //$('details').details();
-  });
-}(jQuery));
