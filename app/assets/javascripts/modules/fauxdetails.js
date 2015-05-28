9df3019fbf4380355b5996cb35f96c4dbff45e3e
Colm Britton <colmjude@gmail.com>
2015-05-28 12:16:52 +0100

    collapsable filtering

---
+++ app/assets/javascripts/modules/fauxdetails.js
@@ -0,0 +1,9 @@
+;(function($) {
+  $(function() {
+      // Add conditional classname based on support
+      //$('html').addClass($.fn.details.support ? 'details' : 'no-details');
+
+      // Emulate <details> where necessary and enable open/close event handlers
+      //$('details').details();
+  });
+}(jQuery));
